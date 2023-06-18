cca8758b408875bee1468fca0b9da7093ecc2703
FWiman <frewim@gmail.com>
2023-06-19 00:28:39 +0200

    Added logo + background

--- MovieApp/src/Components/Navbar.tsx
+++ MovieApp/src/Components/Navbar.tsx
@@ -2,26 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaEnvelope, FaHome, FaInfo, FaSearch } from "react-icons/fa";
 import "../Css/Navbar.css";
-import logo from "../assets/logo-placeholder.png";
+import logo from "../assets/passale3.png";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="nav-left">
         <Link to="/" className="nav-link">
-          <FaHome size={30} />
+          <FaHome size={30} color={"#e1cc92"} />
         </Link>
         <Link to="/search" className="nav-link">
-          <FaSearch size={30} />
+          <FaSearch size={30} color={"#e1cc92"} />
         </Link>
+        <img src={logo} alt="Logo" className="logo" />
       </div>
-      <img src={logo} alt="Logo" className="logo" />
       <div className="nav-right">
         <Link to="/about" className="nav-link">
-          <FaInfo size={30} />
+          <FaInfo size={30} color={"#e1cc92"} />
         </Link>
         <Link to="/contact" className="nav-link">
-          <FaEnvelope size={30} />
+          <FaEnvelope size={30} color={"#e1cc92"} />
         </Link>
       </div>
     </nav>
